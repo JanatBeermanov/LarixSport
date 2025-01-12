f34766ed49f8720b962a08451862bdfd70e70a1d
JanatBeermanov <ajojan90@gmail.com>
2025-01-12 13:11:58 +0500

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBdFEXBl_d1N_Qt_e5h-RqA3EU5Cljovgo",
